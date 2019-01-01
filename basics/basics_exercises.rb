387626a045f1454612e06c5c844db942b97efb93
Melissa Harvey <melissawharvey@gmail.com>
2018-12-31 21:16:06 -0600

    Complete basics chapter exercises

---
+++ basics/basics_exercises.rb
@@ -0,0 +1,45 @@
+# 1 - Add 2 strings together
+"Melissa" + "Harvey"
+
+
+# 2 - Modulo, division, or both
+thousand = 5412 / 1000
+hundred = 5412 % 1000 / 10
+ten = 5412 % 100 / 10
+one = 5412 % 10
+
+
+# 3 - Movie program
+movies = {
+  jaws: "1975",
+  harry_potter_azkaban: "2004",
+  man_of_steel: "2013",
+  shrek: "2001",
+  halloween_2: "1981"
+}
+
+movies.each { |t, y| puts y }
+
+
+# 4 - Dates in array
+movie_dates = ["1975", "2004", "2013", "2001", "1981"]
+
+puts movie_dates
+
+
+# 5 - Factorial
+puts 5 * 4 * 3 * 2 * 1
+puts 6 * 5 * 4 * 3 * 2 * 1
+puts 7 * 6 * 5 * 4 * 3 * 2 * 1
+puts 8 * 7 * 6 * 5 * 4 * 3 * 2 * 1
+
+
+# 6 - Squares of floats
+puts 3.5 ** 2
+puts 2.0 ** 2
+puts 10.5 ** 2
+
+
+# 7 - Error
+# There is no closing bracket somewhere in your code.
+
