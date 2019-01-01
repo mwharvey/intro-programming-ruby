671ec5f32950ffbc5afe88e0d2aaea435abe79b7
Melissa Harvey <melissawharvey@gmail.com>
2019-01-01 13:48:23 -0600

    Finish exercises for variables chapter

---
+++ variables/variables_exercises.rb
@@ -0,0 +1,33 @@
+# name.rb
+puts = "Enter your name."
+name = gets.chomp 
+puts = "Hi, #{name}"
+
+# age.rb
+puts = "How old are you?"
+age = gets.chomp.to_i
+
+puts "In 10 years, you will be: " 
+puts age + 10
+puts "In 20 years, you will be: " 
+puts age + 20
+puts "in 30 years, you will be: " 
+puts age + 30
+puts "In 40 years, you will be: " 
+puts age + 40
+
+# name.rb
+10.times do 
+  puts name
+end
+
+# name.rb
+puts "What is your first name?"
+first_name = gets.chomp
+puts "What is your last name?"
+last_name = gets.chomp
+puts "Hi, #{first_name} #{last_name}!"
+
+# Question 5 - what does x print to screen
+3
+# nothing because x is not available outside the block
